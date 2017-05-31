249acd597a187bb00461cb9b4bbea24324cc3bdb
liam <liamstabeno@gmail.com>
2017-05-31 15:29:46 -0700

    add a placeholder array and displays array with the listView on the findBeer activity

--- app/src/main/java/com/example/guest/beerhere/FindBeer.java
+++ app/src/main/java/com/example/guest/beerhere/FindBeer.java
@@ -3,6 +3,8 @@ package com.example.guest.beerhere;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -10,13 +12,23 @@ import butterknife.ButterKnife;
 
 public class FindBeer extends AppCompatActivity {
     @Bind(R.id.locationTextView) TextView locationText; //  This is the text view at the top of the screen, will be changed once we can make API calls.
-
+    @Bind(R.id.listView)
+    ListView beerList;
+    private String[] Beers = new String[] {"Ale", "Lager",  //  This array is a placeholder for now, not sure if we will query the api by beer type, not sure where this little app is headed just yet
+            "Pilsner", "Stout", "Pilsner", "Porter",
+            "Bock", "Weissbier", "Lambic", "Kölsch",
+            "Malt Liquor"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
         ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_activated_1, Beers);  // Creates a new arrayadapter named adapter
+        beerList.setAdapter(adapter);  // Feeds the beerList listView on the layout the array
+
+
 
         Intent intent = getIntent();  //  Gets the string from the user from the main activity intent.
         String location = intent.getStringExtra("zip");// creates a new string to put in the message.
