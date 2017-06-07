0cb4ff32038f973d059f3b9fd1c193a80ca64a90
Guest User <Guest@Epicodus-7F.local>
2017-06-07 14:15:56 -0700

    fix random bug

--- app/src/main/java/com/example/guest/beerhere/Models/Brewery.java
+++ app/src/main/java/com/example/guest/beerhere/Models/Brewery.java
@@ -73,4 +73,5 @@ public class Brewery {
             return mClosed;
         }
 
+
 }
--- app/src/main/java/com/example/guest/beerhere/ui/BreweryDetailFragment.java
+++ app/src/main/java/com/example/guest/beerhere/ui/BreweryDetailFragment.java
@@ -7,103 +7,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.guest.beerhere.Models.Brewery;
 import com.example.guest.beerhere.R;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BreweryDetailFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BreweryDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.parceler.Parcels;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class BreweryDetailFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @Bind(R.id.breweryImageView) ImageView mImageLabel;
+    @Bind(R.id.breweryNameTextView) TextView mNameLabel;
+    @Bind(R.id.phoneTextView) TextView mPhoneLabel;
+    @Bind(R.id.addressTextView) TextView mAddressLabel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Brewery mBrewery;
 
-    private OnFragmentInteractionListener mListener;
-
-    public BreweryDetailFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BreweryDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BreweryDetailFragment newInstance(String param1, String param2) {
-        BreweryDetailFragment fragment = new BreweryDetailFragment();
+    public static BreweryDetailFragment newInstance(Brewery brewery) {
+        BreweryDetailFragment breweryDetailFragment = new BreweryDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putParcelable("brewery", Parcels.wrap(brewery));
+        breweryDetailFragment.setArguments(args);
+        return breweryDetailFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mBrewery = Parcels.unwrap(getArguments().getParcelable("brewery"));
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_brewery_detail, container, false);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_brewery_detail, container, false);
+        ButterKnife.bind(this, view);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        Picasso.with(view.getContext()).load(mBrewery.getLogo()).into(mImageLabel);
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        mNameLabel.setText(mBrewery.getName());
+        mPhoneLabel.setText(mBrewery.getPhone());
+        mAddressLabel.setText(mBrewery.getAddress());
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return view;
     }
 }
