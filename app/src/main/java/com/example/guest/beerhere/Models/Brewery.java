0020ef53229ae1d1e5c6b3276f6f41723ac7c48e
Guest User <Guest@Epicodus-5A.local>
2017-06-11 20:22:19 -0700

    adjust model to adhere to firebase naming

--- app/src/main/java/com/example/guest/beerhere/Models/Brewery.java
+++ app/src/main/java/com/example/guest/beerhere/Models/Brewery.java
@@ -9,72 +9,72 @@ import org.parceler.Parcel;
 
 @Parcel
 public class Brewery {
-        private String mName;
-        private String mPhone;
-        private String mWebsite;
-        private String mAddress;
-        private double mLatitude;
-        private double mLongitude;
-        private String mIcon;
-        private String mLogo;
-        private String mCity;
-        private String mState;
-        private String mClosed;
+     String name;
+     String phone;
+     String website;
+     String address;
+     double latitude;
+     double longitude;
+     String icon;
+     String logo;
+     String city;
+     String state;
+     String closed;
 
-        public Brewery() {}
+    public Brewery() {}
 
-        public Brewery(String name, String phone, String website,
-                       String address, double latitude, double longitude,
-                       String icon, String logo, String city, String state, String closed)  {
+    public Brewery(String name, String phone, String website,
+                   String address, double latitude, double longitude,
+                   String icon, String logo, String city, String state, String closed)  {
 
-            this.mName = name;
-            this.mPhone = phone;
-            this.mWebsite = website;
-            this.mAddress = address;
-            this.mLatitude = latitude;
-            this.mLongitude = longitude;
-            this.mIcon = icon;
-            this.mLogo = logo;
-            this.mCity = city;
-            this.mState = state;
-            this.mClosed = closed;
-        }
+        this.name = name;
+        this.phone = phone;
+        this.website = website;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.icon = icon;
+        this.logo = logo;
+        this.city = city;
+        this.state = state;
+        this.closed = closed;
+    }
 
-        public String getName() {
-            return mName;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public String getPhone() {
-            return mPhone;
-        }
+    public String getPhone() {
+        return phone;
+    }
 
-        public String getWebsite() {
-            return  mWebsite;
-        }
+    public String getWebsite() {
+        return  website;
+    }
 
-        public String getAddress() {
-            return (mAddress + ", " + mCity + ", " + mState); //  change the format of this later.
-        }
+    public String getAddress() {
+        return (address + ", " + city + ", " + state); //  change the format of this later.
+    }
 
-        public double getLatitude() {
-            return mLatitude;
-        }
+    public double getLatitude() {
+        return latitude;
+    }
 
-        public double getLongitude() {
-            return mLongitude;
-        }
+    public double getLongitude() {
+        return longitude;
+    }
 
-        public String getIcon() {
-            return mIcon;
-        }
+    public String getIcon() {
+        return icon;
+    }
 
-        public String getLogo() {
-            return mLogo;
-        }
+    public String getLogo() {
+        return logo;
+    }
 
-        public String getClosed() {
-            return mClosed;
-        }
+    public String getClosed() {
+        return closed;
+    }
 
 
 }
