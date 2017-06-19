9f2d4a64c356427a91e724c611e544d2eb69f1e7
Guest User <Guest@Epicodus-6A.local>
2017-06-19 16:22:37 -0700

    add content to the about page

--- app/src/main/java/com/example/guest/beerhere/ui/AboutActivity.java
+++ app/src/main/java/com/example/guest/beerhere/ui/AboutActivity.java
@@ -1,15 +1,39 @@
 package com.example.guest.beerhere.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.guest.beerhere.R;
 
-public class AboutActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.button) Button bio;
+    @Bind(R.id.button2) Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+        back.setOnClickListener(this);
+        bio.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == bio) {
+            Intent intent = new Intent(AboutActivity.this, Bio.class);
+            startActivity(intent);
+        }
+        if (v == back) {
+            Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
