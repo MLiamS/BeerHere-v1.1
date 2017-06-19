6755bce6a08e8792d436fab68d09e87d1bdfc223
Guest User <Guest@Epicodus-6A.local>
2017-06-19 15:36:28 -0700

    add links to linkedin, mail, github

--- app/src/main/java/com/example/guest/beerhere/ui/Bio.java
+++ app/src/main/java/com/example/guest/beerhere/ui/Bio.java
@@ -2,6 +2,7 @@ package com.example.guest.beerhere.ui;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.media.session.IMediaControllerCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
@@ -16,7 +17,7 @@ import com.example.guest.beerhere.Constants;
 import com.example.guest.beerhere.R;
 
 
-public class Bio extends AppCompatActivity implements View.OnClickListener{
+public class Bio extends AppCompatActivity implements View.OnClickListener {
     TextView mTextViewBio;
     ImageButton contact;
     ImageButton gitHub;
@@ -28,13 +29,34 @@ public class Bio extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.activity_bio);
         mTextViewBio = (TextView) findViewById(R.id.textViewBio1);
         mTextViewBio.setOnClickListener(this);
+        contact = (ImageButton) findViewById(R.id.emailButton);
+        contact.setOnClickListener(this);
+        gitHub = (ImageButton) findViewById(R.id.githubButton);
+        gitHub.setOnClickListener(this);
+        linkedin = (ImageButton) findViewById(R.id.linkedinButton);
+        linkedin.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v)  {
+    public void onClick(View v) {
         if (v == mTextViewBio) {
             Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.EURL));
             startActivity(webIntent);
+        }
+        if (v == gitHub) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/mliams"));
+            startActivity(webIntent);
+        }
+        if (v == linkedin) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/liam-stabeno-6a647613b"));
+            startActivity(webIntent);
+        }
+        if (v == contact) {
+
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+            emailIntent.setData(Uri.parse("mailto:liamstabeno@gmail.com"));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Beer Here!");
+            startActivity(emailIntent);
 
         }
     }
