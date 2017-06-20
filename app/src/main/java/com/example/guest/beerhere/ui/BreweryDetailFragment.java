57f6dc9424e6f7171ebbd3b7f37162627de2a9c0
Guest User <Guest@Epicodus-5G.local>
2017-06-20 12:54:38 -0700

    each user will now save breweries to thei own list

--- app/src/main/java/com/example/guest/beerhere/ui/BreweryDetailFragment.java
+++ app/src/main/java/com/example/guest/beerhere/ui/BreweryDetailFragment.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
 import com.example.guest.beerhere.Constants;
 import com.example.guest.beerhere.Models.Brewery;
 import com.example.guest.beerhere.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -94,10 +96,19 @@ public class BreweryDetailFragment extends Fragment implements View.OnClickListe
                 startActivity(mapIntent);
             }
             if (v == mSaveRestaurantButton) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = user.getUid();
+
                 DatabaseReference restaurantRef = FirebaseDatabase
                         .getInstance()
-                        .getReference(Constants.FIREBASE_CHILD_BREWERIES);
-                restaurantRef.push().setValue(mBrewery);
+                        .getReference(Constants.FIREBASE_CHILD_BREWERIES)
+                        .child(uid);
+
+                DatabaseReference pushRef = restaurantRef.push();
+                String pushId = pushRef.getKey();
+                mBrewery.setPushId(pushId);
+                pushRef.setValue(mBrewery);
+
                 Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT).show();
             }
         }
